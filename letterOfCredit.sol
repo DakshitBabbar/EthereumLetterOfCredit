9296674a25ef53b428965ef7ba51d590365daa4e
tushita pandey <72200485+tushitapandey@users.noreply.github.com>
2024-04-24 14:18:34 +0530

    Update letterOfCredit.sol

--- letterOfCredit.sol
+++ letterOfCredit.sol
@@ -1,58 +1,63 @@
-// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
 
-contract LetterOfCredit {
+contract LetterOfCredit 
+{
     enum Status { Pending, Approved, Fulfilled, Rejected }
 
     struct LOC {
         uint id;
-        address buyerBank;
-        address sellerBank;
-        uint amount;
-        Status status;
+        address buyer;
+        address seller;
+        uint amnt;
+        Status status; 
     }
-
-    LOC[] public locs;
+    LOC[] public locs; 
     mapping(uint => address) public locToOwner;
 
-    event LOCRequested(uint locId, address buyerBank, address sellerBank, uint amount);
+    event LOCRequested(uint locId, address buyer, address seller, uint amnt);
     event LOCApproved(uint locId);
     event LOCFulfilled(uint locId);
     event LOCRejected(uint locId);
 
-    function requestLOC(address _sellerBank, uint _amount) external {
+
+    function requestLOC(address _seller, uint _amnt) external 
+    {
         uint id = locs.length;
-        locs.push(LOC(id, msg.sender, _sellerBank, _amount, Status.Pending));
+        locs.push(LOC(id, msg.sender, _seller, _amnt, Status.Pending));
         locToOwner[id] = msg.sender;
-        emit LOCRequested(id, msg.sender, _sellerBank, _amount);
+        emit LOCRequested(id, msg.sender, _seller, _amnt);
     }
 
-    function approveLOC(uint _locId) external {
-        require(_locId < locs.length, "Invalid locId");
-        require(locs[_locId].sellerBank == msg.sender, "Only the seller bank can approve this LOC.");
+    function approveLOC(uint _locId) external 
+    {
+        require(_locId < locs.length, "This LOC does not exist");
+        require(locs[_locId].seller == msg.sender, "Only seller bank can approve the LOC");
         locs[_locId].status = Status.Approved;
         emit LOCApproved(_locId);
     }
-
-    function fulfillLOC(uint _locId) external {
-        require(_locId < locs.length, "Invalid locId");
-        require(locs[_locId].buyerBank == msg.sender, "Only the buyer bank can fulfill this LOC.");
-        require(locs[_locId].status == Status.Approved, "LOC must be approved before it can be fulfilled.");
-        locs[_locId].status = Status.Fulfilled;
-        emit LOCFulfilled(_locId);
-    }
-
-    function rejectLOC(uint _locId) external {
-        require(_locId < locs.length, "Invalid locId");
-        require(locs[_locId].sellerBank == msg.sender, "Only the seller bank can reject this LOC.");
-        require(locs[_locId].status == Status.Approved, "LOC must be approved before it can be rejected.");
+    function rejectLOC(uint _locId) external 
+    {
+        require(_locId < locs.length, "This LOC does not exist");
+        require(locs[_locId].seller == msg.sender, "Only the seller bank can reject the LOC.");
+        require(locs[_locId].status == Status.Approved, "LOC is not approvd yet, contact Seller bank to approve it");
         locs[_locId].status = Status.Rejected;
         emit LOCRejected(_locId);
     }
 
-    function getLOC(uint _locId) public view returns (uint, address, address, uint, Status) {
-        require(_locId < locs.length, "Invalid locId");
+    function fulfillLOC(uint _locId) external
+    {
+        require(_locId < locs.length, "This LOC does not exist");
+        require(locs[_locId].buyer == msg.sender, "Only the buyer bank can fulfill the LOC.");
+        require(locs[_locId].status == Status.Approved, "LOC is not approvd yet, contact Seller bank to approve it");
+        locs[_locId].status = Status.Fulfilled;
+        emit LOCFulfilled(_locId);
+    }
+
+//to get the details of LOC given the loc id
+    function getLOC(uint _locId) public view returns (uint, address, address, uint, Status) 
+    {
+        require(_locId < locs.length, "This LOC does not exist");
         LOC storage loc = locs[_locId];
-        return (loc.id, loc.buyerBank, loc.sellerBank, loc.amount, loc.status);
+        return (loc.id, loc.buyer, loc.seller, loc.amnt, loc.status);
     }
 }
